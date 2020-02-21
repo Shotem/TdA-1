43c8362dc1d2406ab06a749fe9ec52477aca3332
Vicente Garza Reyna <vgarzareyna@gmail.com>
2020-02-21 04:38:36 -0600

    Ultima versión (ahora sí)

--- main.cpp
+++ main.cpp
@@ -4,23 +4,43 @@
 
 using namespace std;
 
-bool automata(string);
+bool automata(string, string);
 void testAutomata(string*);
 
 int main(int argc, char** argv) {
-	string str, confirmation;
+	string str, display, cont;
 	setlocale(LC_ALL, "");
 	
+	cout << "=============== Proyecto 1 - Teoría de Automatas ===============" << endl << endl;
 	
+	cout << "\t --Integrantes:------------------------------ " << endl;
+	cout << "\t| Vicente Garza Reyna              - 1847176 |" << endl;
+	cout << "\t| Diana Lizeth Renteria Aguilera   - 1852889 |" << endl;
+	cout << "\t| Andrés Isaac Montes Bartolo      - 1854017 |" << endl;
+	cout << "\t -------------------------------------------- " << endl << endl;
+	
+	cout << "Presione enter para continuar..." << endl;
+	fflush(stdin);
+	getchar();
 	
 	do {
 		system("cls");
-		cout << "Nombre: Andres Isaac Montes Bartolo\tMatricula: 1854017" << endl << endl;
+		cout << "Nombre usado: Andres Isaac Montes Bartolo\tMatricula usada: 1854017" << endl << endl;
 		cout << "Alfabeto = { 0, 1, 4, 5, 7, 8, a, b, c, d, e, i, l, m, n, o, r, s, t, '.' }" << endl;
 		cout << "Ingrese una cadena de caracteres. Por ejemplo, \"0aimb.1854017\":" << endl;
 		cin >> str;
 		
-		if ( automata(str) ){
+		do{
+			cout << "¿Desea ver lo que hace el autómata? [S/N]" << endl;
+			cin >> display;
+			display[0] = toupper( display[0] );
+			if ( display != "S" && display != "N" ) {
+				cout << "Favor de ingresar una respuesta válida" << endl << endl;
+			}
+		} while ( display != "S" && display != "N" );
+		cout << endl;
+		
+		if ( automata(str, display) ){
 			cout << "Cadena valida" << endl;
 		} else {
 			cout << "Cadena invalida" << endl;
@@ -28,25 +48,25 @@ int main(int argc, char** argv) {
 		
 		do{
 			cout << "¿Desea continuar ingresando cadenas? [S/N]" << endl;
-			cin >> confirmation;
-			confirmation[0] = toupper( confirmation[0] );
-			if (confirmation != "S" && confirmation!= "N") {
+			cin >> cont;
+			cont[0] = toupper( cont[0] );
+			if ( cont != "S" && cont != "N") {
 				cout << "Favor de ingresar una respuesta válida" << endl << endl;
 			}
-		} while (confirmation != "S" && confirmation!= "N");
+		} while ( cont != "S" && cont != "N");
 		
 		
-	} while ( confirmation == "S");
+	} while ( cont == "S");
 	
 	cout << "Gracias por probar este programa." << endl << endl;
 	cout << "Puede encontrar el código fuente en:" << endl << "https://github.com/Shotem/TdA-1" << endl << endl;
-	cout << "Presione cualquier tecla para continuar..." << endl;
+	cout << "Presione enter para continuar..." << endl;
 	fflush(stdin);
 	getchar();
 	
 }
 
-bool automata(string str) {
+bool automata(string str, string display) {
 	typedef enum States{ q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, qA, qB, qC, qD, qE, qf, qm, states} States;
 	int chars = 20;
 	
@@ -67,7 +87,7 @@ bool automata(string str) {
 		{  qE, qD, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE }, //qC
 		{  qE, qE, qE, qE, qf, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE }, //qD
 		{  qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, q7 }, //qE
-		{  qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE }, //qf
+		{  qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, qE, q7 }, //qf
 		{  qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm, qm }  //qm
 	};
 	
@@ -75,6 +95,9 @@ bool automata(string str) {
 	States curr_state = q0;
 	
 	for (int i = 0; i < str.length(); i++) {
+		if (display == "S"){
+			cout << "Estado: " << curr_state << "\tCaracter: " << str[i] << endl;	
+		}
 		switch(str[i]) {
 			case '0':
 				curr_state = matrix[curr_state][0];
@@ -144,10 +167,15 @@ bool automata(string str) {
 		}
 		
 		if (curr_state == qm) {
+			if (display == "S"){
+				cout << "Se alcanzó el estado muerto (" << qm << ")" << endl;
+			}
 			break;
 		}
 	}
-	
+	if (display == "S"){
+		cout << "Estado: " << curr_state << endl << endl;
+	}
 	return (curr_state == qf);
 	
 }
